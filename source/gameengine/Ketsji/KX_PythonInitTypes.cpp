17cef1119804781fa74c2366bec951a625dc6370
Ulysse Martin <youle31@users.noreply.github.com>
2018-04-11 07:46:35 +0200

    move State Actuator to GameLogic

--- source/gameengine/Ketsji/KX_PythonInitTypes.cpp
+++ source/gameengine/Ketsji/KX_PythonInitTypes.cpp
@@ -69,7 +69,7 @@
 #include "SCA_EndObjectActuator.h"
 #include "SCA_ReplaceMeshActuator.h"
 #include "KX_SceneActuator.h"
-#include "KX_StateActuator.h"
+#include "SCA_StateActuator.h"
 #include "SCA_SteeringActuator.h"
 #include "SCA_TrackToActuator.h"
 #include "KX_VehicleWrapper.h"
@@ -248,7 +248,7 @@ PyMODINIT_FUNC initGameTypesPythonBinding(void)
 		PyType_Ready_Attr(dict, KX_NavMeshObject, init_getset);
 		PyType_Ready_Attr(dict, KX_SceneActuator, init_getset);
 		PyType_Ready_Attr(dict, SCA_SoundActuator, init_getset);
-		PyType_Ready_Attr(dict, KX_StateActuator, init_getset);
+		PyType_Ready_Attr(dict, SCA_StateActuator, init_getset);
 		PyType_Ready_Attr(dict, SCA_SteeringActuator, init_getset);
 		PyType_Ready_Attr(dict, SCA_CollisionSensor, init_getset);
 		PyType_Ready_Attr(dict, SCA_TrackToActuator, init_getset);
