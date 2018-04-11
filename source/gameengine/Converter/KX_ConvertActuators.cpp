363c4f6ae1d586fbeb1096c52bc9aa1d299e81bd
Ulysse Martin <youle31@users.noreply.github.com>
2018-04-11 06:58:30 +0200

    move AddObjectActuator to GameLogic

--- source/gameengine/Converter/KX_ConvertActuators.cpp
+++ source/gameengine/Converter/KX_ConvertActuators.cpp
@@ -65,7 +65,7 @@
 #include "SCA_GameActuator.h"
 #include "KX_StateActuator.h"
 #include "KX_VisibilityActuator.h"
-#include "KX_SCA_AddObjectActuator.h"
+#include "SCA_AddObjectActuator.h"
 #include "KX_SCA_EndObjectActuator.h"
 #include "KX_SCA_ReplaceMeshActuator.h"
 #include "SCA_ParentActuator.h"
@@ -439,7 +439,7 @@ void BL_ConvertActuators(const char* maggiename,
 							}
 						}
 						
-						KX_SCA_AddObjectActuator* tmpaddact = new KX_SCA_AddObjectActuator(
+						SCA_AddObjectActuator* tmpaddact = new SCA_AddObjectActuator(
 						            gameobj,
 						            originalval,
 						            editobact->time,
